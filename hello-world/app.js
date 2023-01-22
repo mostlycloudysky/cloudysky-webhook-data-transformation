2d2fc2e80f9524f17193197fa4bba49a59b68f31
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-01-21 21:44:18 -0500

    more updates

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,6 +1,7 @@
 // const axios = require('axios')
 // const url = 'http://checkip.amazonaws.com/';
 let response;
+const toMarkdown = require('@sanity/block-content-to-markdown');
 
 /**
  *
@@ -16,6 +17,25 @@ let response;
  */
 exports.lambdaHandler = async (event, context) => {
   console.log(event);
+
+  let blog = JSON.parse(event.body);
+  let blogTitle = blog.title;
+  let blogDescription = blog.description;
+  console.log('Blog Title', blogTitle);
+  console.log('Blog Description', blogDescription);
+  let blogBody = blog.body;
+  console.log('==================');
+  // console.log(JSON.stringify(blogBody));
+
+  const serializers = {
+    types: {
+      codeBlock: (props) =>
+        '```' + props.node.language + '\n' + props.node.code + '\n```',
+    },
+  };
+
+  console.log(toMarkdown(blogBody, { serializers }));
+
   try {
     // const ret = await axios(url);
     response = {
