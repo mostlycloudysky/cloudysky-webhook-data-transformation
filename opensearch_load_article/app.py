bf1998630dfcf88a98573afec5fdc8b5d6231b82
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-02-20 16:37:05 -0500

    add code to read secret manager data

--- opensearch_load_article/app.py
+++ opensearch_load_article/app.py
@@ -1,6 +1,27 @@
 import json
+import boto3
+
 
 def lambda_handler(event, context):
 
     sanity_blog_playload = event['Payload']
     print(sanity_blog_playload)
+
+    # Get Secret from secret manager
+    def get_secrets(secret_name):
+        region_name = "us-east-1"
+        session = boto3.session.Session()
+        client = session.client(
+            service_name='secretsmanager',
+            region_name=region_name
+        )
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name)
+        return get_secret_value_response
+
+    # Execution steps
+    user_name = get_secrets(secret_name="os-username")['SecretString']
+    password = get_secrets(secret_name="os-password")['SecretString']
+
+    print(user_name)
+    print(password)
